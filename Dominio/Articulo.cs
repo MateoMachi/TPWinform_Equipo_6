1e05b1df1f2e00fe92b120b55d0469b76bc94b25
unknown <macchimateo@gmail.com>
2023-09-10 22:36:22 -0300

    Creacion de Clase Articulo, Marca, Categoria. Estructura en Capas

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string codigoArticulo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Marca Marca { get; set; }
+    
+        public Categoria Categoria { get; set; }
+      
+        public Imagen imagen { get; set; }
+        public decimal precio { get; set; }
+
+    }
+}
+
+
+
---
+++ Dominio/Marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        public int idMarca { get; set; }
+        public string marca { get; set; }
+
+        public override string ToString()
+        {
+            return marca;
+        }
+
+    }
+}
